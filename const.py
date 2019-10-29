074d97e2492f8e345dc674f89033d2024aaeaf23
nzy719 <807025974@qq.com>
2019-10-29 23:01:12 +0800

    自定义常量类 -Const

---
+++ const.py
@@ -0,0 +1,18 @@
+# 类名: Const
+# 描述：自定义常量类，用于补充python默认不支持常量的缺陷，常量命名必须全为大写字母
+
+class Const():
+    class ConstError(TypeError):
+        pass
+
+    class ConstNameError(ConstError):
+        pass
+
+    def __setattr__(self, key, value):
+        if key in self.__dict__.keys():
+            raise self.ConstError("Can't change the value of the constant: " + '\'' + key + '\'')
+
+        if not key.isupper():
+            raise self.ConstNameError("The constant must be named with upper letters: " + '\'' + key + '\'')
+
+        self.__dict__[key] = value
